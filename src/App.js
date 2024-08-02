e2a56304c8bd98f82a148c09fac2396d6849965f
djjung <ekwls8460@gmail.com>
2024-08-02 20:21:43 +0900

    Cleanup 학습

--- src/App.js
+++ src/App.js
@@ -1,40 +1,32 @@
 import { useState, useEffect } from "react";
 
-function App() {
-  const [counter, setValue] = useState(0);
-  const [keyword, setKeyword] = useState("");
-  const onClick = () => setValue((prev) => prev + 1);
-  const onChange = (event) => setKeyword(event.target.value);
-  /*
-  useEffect : 특정한 코드만 변화했을 때 원하는 코드들을 실행할 수 있는 방법
-  첫번째 인자에는 실행할 함수
-  두번째 인자에는 변화를 지켜볼 인자
-  []자리에 keyword 써줌 -> keyword가 변화할 때 코드를 실행할 거라고 react.js에게 알려주는 것.
-  이것이 바로 빈 array를 써주었을 때 코드가 단 한번만 실행되는 이유임
-  react가 지켜볼 게 아무것도 없으니 처음 한번만 실행되는 것
-  */
+/*
+useEffect의 subscription을 종료할 때 
+즉, 컴포넌트가 unmount될 때 실행할 로직은 useEffect의 EffectCallback에서 반환되는 함수로 구현한다. 
+여기서 반환되는 함수를 cleanup function이라고 부르고 메모리 누수를 방지하기 위해 사용된다. 
+*/
+
+function Hello() {
   useEffect(() => {
-    console.log("I run only once");
+    console.log("Hi :)");
+    return () => console.log("Bye :(");
   }, []);
-  useEffect(() => {
-    console.log("I run when 'keyword' changes.");
-  }, [keyword]);
-  useEffect(() => {
-    console.log("I run when 'counter' changes.");
-  }, [counter]);
-  useEffect(() => {
-    console.log("I run when keyword & counter changes.");
-  }, [keyword, counter]);
+  // useEffect(function () {
+  //   console.log("Hi :)");
+  //   return function () {
+  //     console.log("Bye :(");
+  //   };
+  // }, []);
+  return <h1>Hello</h1>;
+}
+
+function App() {
+  const [showing, setShowing] = useState(false);
+  const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input
-        value={keyword}
-        onChange={onChange}
-        type="text"
-        placeholder="Search here.."
-      />
-      <h1>{counter}</h1>
-      <button onClick={onClick}>click me</button>
+      {showing ? <Hello /> : null}
+      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   );
 }
