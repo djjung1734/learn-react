17efa3838c3890dee4c924805315a944c2d637dd
djjung <ekwls8460@gmail.com>
2024-08-05 00:10:56 +0900

    TODO List 생성

--- src/App.js
+++ src/App.js
@@ -1,32 +1,33 @@
 import { useState, useEffect } from "react";
 
-/*
-useEffect의 subscription을 종료할 때 
-즉, 컴포넌트가 unmount될 때 실행할 로직은 useEffect의 EffectCallback에서 반환되는 함수로 구현한다. 
-여기서 반환되는 함수를 cleanup function이라고 부르고 메모리 누수를 방지하기 위해 사용된다. 
-*/
-
-function Hello() {
-  useEffect(() => {
-    console.log("Hi :)");
-    return () => console.log("Bye :(");
-  }, []);
-  // useEffect(function () {
-  //   console.log("Hi :)");
-  //   return function () {
-  //     console.log("Bye :(");
-  //   };
-  // }, []);
-  return <h1>Hello</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  };
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr />
+      {toDos.map((toDo, index) => (
+        <li key={index}>{toDo}</li>
+      ))}
     </div>
   );
 }
