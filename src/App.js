4db870c765e6589ad2de5873627b5c3180803d0e
djjung <ekwls8460@gmail.com>
2024-08-05 17:01:53 +0900

    Coin Tracker 응용
    - 달러를 입력하고 코인으로 얼마나 바꿀 수 있는지 나타내기

--- src/App.js
+++ src/App.js
@@ -3,6 +3,12 @@ import { useState, useEffect } from "react";
 function App() {
   const [loading, setLoading] = useState(true);
   const [coins, setCoins] = useState([]);
+  const [selected, setSelected] = useState();
+  const [money, setMoney] = useState();
+  const onSelect = (event) => setSelected(event.target.value);
+  const onChange = (event) => setMoney(event.target.value);
+  console.log(money);
+  console.log(selected);
   useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
@@ -17,16 +23,30 @@ function App() {
       {loading ? (
         <strong>Loading...</strong>
       ) : (
-        <select>
+        <select value={selected} onChange={onSelect}>
           {coins.map((coin, index) => (
-            <option key={index}>
+            <option value={coin.quotes.USD.price} key={index}>
               {coin.name}({coin.symbol}) : ${coin.quotes.USD.price} USD
             </option>
           ))}
         </select>
       )}
+      {selected ? (
+        <div>
+          <input
+            onChange={onChange}
+            value={money}
+            type="number"
+            placeholder="Input your money"
+          />
+          <h3>Dollars to Coins : {money / selected}</h3>
+        </div>
+      ) : null}
     </div>
   );
 }
 
 export default App;
+
+//fetch 공부해보기
+//input 만들어서 내가 들고있는 달러로 얼마의 코인을 살수 있는지 만들어보기
